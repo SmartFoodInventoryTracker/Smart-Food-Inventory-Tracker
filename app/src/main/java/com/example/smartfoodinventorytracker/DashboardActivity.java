9d23904988cbab8d2b4ffa7cc046d1a3c9da7a38
c-moha <mo.oubagha@gmail.com>
2025-02-24 16:15:31 -0500

    test

--- app/src/main/java/com/example/smartfoodinventorytracker/DashboardActivity.java
+++ app/src/main/java/com/example/smartfoodinventorytracker/DashboardActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
+
 
     private DrawerLayout drawerLayout;
     private TextView greetingText;
@@ -69,14 +71,14 @@ public class DashboardActivity extends AppCompatActivity {
         // ✅ Dashboard Button Click Listeners
         inventoryButton.setOnClickListener(v -> {
             Toast.makeText(this, "Inventory Clicked", Toast.LENGTH_SHORT).show();
-            // Intent intent = new Intent(this, InventoryActivity.class);
-            // startActivity(intent);
+            Intent intent = new Intent(this, Inventory.class);
+            startActivity(intent);
         });
 
         shoppingListButton.setOnClickListener(v -> {
             Toast.makeText(this, "Shopping Lists Clicked", Toast.LENGTH_SHORT).show();
-            // Intent intent = new Intent(this, ShoppingListActivity.class);
-            // startActivity(intent);
+            //Intent intent = new Intent(this, ShoppingListActivity.class);
+            //startActivity(intent);
         });
 
         fridgeConditionButton.setOnClickListener(v -> {
