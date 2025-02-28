c7bae1b39f0b89b9d5eef6263b479a10747f0d03
Kevin Mandiouba <mandioubakm@gmail.com>
2025-02-27 22:01:58 -0500

    feat: Add Notification Center, Inventory, Shopping List, and Fridge Condition navigation
    - Added `NotificationCenter` activity and layout.
    - Added `Inventory`, `ShoppingList`, and `FridgeConditions` navigation links from the dashboard menu.
    - Added a new file `.idea/appInsightsSettings.xml`.
    - Updated `AndroidManifest.xml` with `NotificationCenter` activity.
    - Minor code formatting changes.

--- app/src/main/java/com/example/smartfoodinventorytracker/NotificationCenter.java
+++ app/src/main/java/com/example/smartfoodinventorytracker/NotificationCenter.java
@@ -9,13 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Inventory extends AppCompatActivity {
+public class NotificationCenter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inventory);
+        setContentView(R.layout.activity_notification_center);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
