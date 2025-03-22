15b2bb11521c0ed1234fe9ad4e324fcdbbbd9845
Kevin Mandiouba <mandioubakm@gmail.com>
2025-03-21 20:23:45 -0400

    Apply window insets and add view ID

--- app/src/main/java/com/example/smartfoodinventorytracker/OnboardingActivity.java
+++ app/src/main/java/com/example/smartfoodinventorytracker/OnboardingActivity.java
@@ -7,7 +7,12 @@ import android.os.Looper;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class OnboardingActivity extends AppCompatActivity {
 
@@ -34,6 +39,13 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+        EdgeToEdge.enable(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // ✅ Link UI elements
         rotatingTitle = findViewById(R.id.rotatingTitle);
