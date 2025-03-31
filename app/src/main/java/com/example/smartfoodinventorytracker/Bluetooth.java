1acb0f16610056c1272cc41ee352bd9029342786
c-moha <mo.oubagha@gmail.com>
2025-03-31 01:17:51 -0400

    Initialized the bluetooth adapter/connection

--- app/src/main/java/com/example/smartfoodinventorytracker/Bluetooth.java
+++ app/src/main/java/com/example/smartfoodinventorytracker/Bluetooth.java
@@ -1,7 +1,29 @@
 package com.example.smartfoodinventorytracker;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+
+import java.util.Set;
+
 public class Bluetooth {
-    public Bluetooth() {}
+    private Set<Bluetooth> setB;
+    private BluetoothAdapter bt;
+    private BluetoothDevice esp32;
+    private BluetoothSocket channel;
+    public Bluetooth() {
+        bt = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    private void getDevices(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        bt.getBondedDevices();
+    }
 
 
 
