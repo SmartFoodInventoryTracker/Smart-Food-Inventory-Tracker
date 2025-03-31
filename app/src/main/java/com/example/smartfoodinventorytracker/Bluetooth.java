3a95c2a9b471044d3a0ad52e0bf8ecf4fa541df0
c-moha <mo.oubagha@gmail.com>
2025-03-31 01:28:59 -0400

    Enabled the fetching of bluetooth devices connected to user's device

--- app/src/main/java/com/example/smartfoodinventorytracker/Bluetooth.java
+++ app/src/main/java/com/example/smartfoodinventorytracker/Bluetooth.java
@@ -3,6 +3,7 @@ package com.example.smartfoodinventorytracker;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.pm.PackageManager;
 
 import androidx.core.app.ActivityCompat;
@@ -10,20 +11,38 @@ import androidx.core.app.ActivityCompat;
 import java.util.Set;
 
 public class Bluetooth {
-    private Set<Bluetooth> setB;
+    private Set<BluetoothDevice> fetchedB, actual;
     private BluetoothAdapter bt;
     private BluetoothDevice esp32;
     private BluetoothSocket channel;
-    public Bluetooth() {
+
+    private Context cont;
+    public Bluetooth(Context context) {
         bt = BluetoothAdapter.getDefaultAdapter();
+        cont=context;
     }
 
     private void getDevices(){
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(cont, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        bt.getBondedDevices();
+        fetchedB=bt.getBondedDevices();
+        for(BluetoothDevice device:fetchedB)
+        {
+            if(device.getName().contains("ESP32"){
+                actual.add(device);
+            }
+        }
     }
+
+
 
 
 
